9a47cb7df208ade362f3e7c729b451b086c465c9
Pradeep241094 <pradeepprakash24@gmail.com>
2021-05-19 15:02:53 -0400

    Added Timestamp for Each Block of execution in PBFT across various stages

---
+++ validators.js
@@ -0,0 +1,29 @@
+// Import the wallet class
+const Wallet = require("./wallet");
+
+class Validators {
+  // constructor will take an argument which is the number of nodes in the network
+  constructor(numberOfValidators) {
+    this.list = this.generateAddresses(numberOfValidators);
+  }
+
+  // This function generates wallets and their public key
+  // The secret key has been known for demonstration purposes
+  // Secret will be passed from command line to generate the same wallet again
+  // As a result the same public key will be generatedd
+  generateAddresses(numberOfValidators) {
+    console.log('>>>>>>>>>>>>>>>>>>>Number of Validators', numberOfValidators)
+    let list = [];
+    for (let i = 0; i < numberOfValidators; i++) {
+      list.push(new Wallet("NODE" + i).getPublicKey());
+    }
+    console.log('>>>>>>>>>list of valid addresses', list)
+    return list;
+  }
+
+  // This function verfies if the passed public key is a known validator or not
+  isValidValidator(validator) {
+    return this.list.includes(validator);
+  }
+}
+module.exports = Validators;
